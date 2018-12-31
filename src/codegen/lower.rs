63e3504cdbcce1fd109852140cbcc2c5590645d3
Phil Ellison <phil.j.ellison@gmail.com>
2018-12-31 16:38:56 +0000

    use symbol table in more places

--- src/codegen/lower.rs
+++ src/codegen/lower.rs
@@ -50,17 +50,14 @@ pub fn lower_access(
     builder: &Builder,
     // e.g. in(3 * (x + 1) - y, 2 * x)
     access: &Access,
-    // i8* pointing to start of input image buffer
-    input: LLVMValueRef,
     // i32, width of input image
     width: LLVMValueRef,
-    // i32, current value of x variable
-    x: LLVMValueRef,
-    // i32, current value of y variable
-    y: LLVMValueRef
+    symbols: &mut SymbolTable
     // return value has type i8
 ) -> LLVMValueRef {
-    assert_eq!(access.source, "in");
+    let input  = symbols.get(&access.source);
+    let x = symbols.get("x");
+    let y = symbols.get("y");
     let (x, y) = (
         lower_var_expr(builder, &access.x, x, y),
         lower_var_expr(builder, &access.y, x, y)
@@ -74,62 +71,48 @@ pub fn lower_definition(
     builder: &Builder,
     // e.g. in(x, y) + in(x, y - 1)
     definition: &Definition,
-    // i8* pointing to start of input image buffer
-    input: LLVMValueRef,
-    // i32, width of input image
     width: LLVMValueRef,
-    // i32, current value of x variable
-    x: LLVMValueRef,
-    // i32, current value of y variable
-    y: LLVMValueRef
+    symbols: &mut SymbolTable
     // return value has type i8
 ) -> LLVMValueRef {
     match definition {
-        Definition::Access(a) => lower_access(builder, a, input, width, x, y),
+        Definition::Access(a) => lower_access(builder, a, width, symbols),
         Definition::Const(c) => builder.const_i8(*c),
         Definition::Add(l, r) => {
-            let left = lower_definition(builder, l, input, width, x, y);
-            let right = lower_definition(builder, r, input, width, x, y);
+            let left = lower_definition(builder, l, width, symbols);
+            let right = lower_definition(builder, r, width, symbols);
             builder.add(left, right)
         },
         Definition::Mul(l, r) => {
-            let left = lower_definition(builder, l, input, width, x, y);
-            let right = lower_definition(builder, r, input, width, x, y);
+            let left = lower_definition(builder, l, width, symbols);
+            let right = lower_definition(builder, r, width, symbols);
             builder.mul(left, right)
         },
         Definition::Sub(l, r) => {
-            let left = lower_definition(builder, l, input, width, x, y);
-            let right = lower_definition(builder, r, input, width, x, y);
+            let left = lower_definition(builder, l, width, symbols);
+            let right = lower_definition(builder, r, width, symbols);
             builder.sub(left, right)
         },
         Definition::Div(l, r) => {
-            let left = lower_definition(builder, l, input, width, x, y);
-            let right = lower_definition(builder, r, input, width, x, y);
+            let left = lower_definition(builder, l, width, symbols);
+            let right = lower_definition(builder, r, width, symbols);
             builder.sdiv(left, right)
         }
     }
 }
 
-/// Only support reading from source "in" and writing to "out"
 pub fn lower_func(
     builder: &Builder,
     // e.g. out(x, y) = in(x, y) + in(x, y - 1)
     func: &Func,
-    // i8* pointing to start of input image buffer
-    input: LLVMValueRef,
-    // i8* pointing to start of output image buffer
-    output: LLVMValueRef,
     // i32, width of input image
     width: LLVMValueRef,
-    // i32, current value of x variable
-    x: LLVMValueRef,
-    // i32, current value of y variable
-    y: LLVMValueRef
+    // must contain symbols for all mentioned images and variables
+    symbols: &mut SymbolTable
 ) {
-    assert_eq!(func.name, "out");
-    let val = lower_definition(builder, &func.definition, input, width, x, y);
-    let offset = builder.add(builder.mul(y, width), x);
-    let ptr = builder.in_bounds_gep(output, offset);
+    let val = lower_definition(builder, &func.definition, width, symbols);
+    let offset = builder.add(builder.mul(symbols.get("y"), width), symbols.get("x"));
+    let ptr = builder.in_bounds_gep(symbols.get(&func.name), offset);
     builder.store(val, ptr, 1);
 }
 
@@ -190,22 +173,16 @@ pub fn create_process_image_module(context: &Context, func: &Func) -> Module {
     let y_max = builder.trunc(src_height, builder.type_i32());
     let x_max = builder.trunc(src_width, builder.type_i32());
 
-    let generate_x_body = |symbols: &mut SymbolTable| {
-        lower_func(
-            &builder,
-            func,
-            src,
-            dst,
-            x_max,
-            symbols.get("x"),
-            symbols.get("y")
-        );
+    let generate_x_body = |symbols| {
+        lower_func(&builder, func, x_max, symbols);
     };
     let generate_y_body = |symbols| {
         generate_loop(&builder, "x", x_max, llvm_func, symbols, generate_x_body);
     };
 
     let mut symbols = SymbolTable::new();
+    symbols.add("in", src);
+    symbols.add("out", dst);
     generate_loop(&builder, "y", y_max, llvm_func, &mut symbols, generate_y_body);
 
     builder.ret_void();
