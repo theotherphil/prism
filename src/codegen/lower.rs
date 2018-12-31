dc1525635f2f502c8bd4c405fd31d39013254eb1
Phil Ellison <phil.j.ellison@gmail.com>
2018-12-31 15:17:19 +0000

    make compile_process_image_module less readable by extracting y loop generation and x loop generation into separate functions. This is useful as it's a tiny step towards automatically synthesising loop nests in IR

--- src/codegen/lower.rs
+++ src/codegen/lower.rs
@@ -1,6 +1,7 @@
 //! Functions for lowering the prism AST to LLVM IR
 
 use llvm::prelude::*;
+use std::collections::HashMap;
 use crate::codegen::builder::*;
 use crate::codegen::compile::*;
 use crate::ast::*;
@@ -132,11 +133,40 @@ pub fn lower_func(
     builder.store(val, ptr, 1);
 }
 
+#[derive(Debug)]
+pub struct SymbolTable {
+    symbols: HashMap<String, LLVMValueRef>
+}
+
+impl SymbolTable {
+    pub fn new() -> SymbolTable {
+        SymbolTable { symbols: HashMap::new() }
+    }
+
+    pub fn add(&mut self, name: &str, value: LLVMValueRef) {
+        self.symbols.insert(name.to_string(), value);
+    }
+
+    pub fn remove(&mut self, name: &str) {
+        match self.symbols.remove(name) {
+            None => panic!("Remove failed - symbol {} not found", name),
+            _ => {}
+        };
+    }
+
+    pub fn get(&self, name: &str) -> LLVMValueRef {
+        match self.symbols.get(name) {
+            Some(v) => *v,
+            None => panic!("Get failed - symbol {} not found", name)
+        }
+    }
+}
+
 pub fn create_process_image_module(context: &Context, func: &Func) -> Module {
     let module = context.new_module("process_image");
     let builder = Builder::new(context);
 
-    let function_type = builder.func_type(
+    let llvm_func_type = builder.func_type(
         builder.type_void(),
         &mut [
             builder.type_i8_ptr(),
@@ -147,27 +177,53 @@ pub fn create_process_image_module(context: &Context, func: &Func) -> Module {
             builder.type_i64()
         ]
     );
-    let function = builder.add_func(module, "process_image", function_type);
-    let params = builder.get_params(function);
+    let llvm_func = builder.add_func(module, "process_image", llvm_func_type);
+    let params = builder.get_params(llvm_func);
     // We currently just assume that src and dst have the same dimensions
     // so ignore the last two params
     let (src, src_width, src_height, dst) = (
         params[0], params[1], params[2], params[3]
     );
 
-    let entry = builder.new_block(function, "entry");
-    let y_header = builder.new_block(function, "y.header");
-    let y_loop = builder.new_block(function, "y.loop");
-    let y_after = builder.new_block(function, "y.after");
-    let x_header = builder.new_block(function, "x.header");
-    let x_loop = builder.new_block(function, "x.loop");
-    let x_after = builder.new_block(function, "x.after");
+    let mut symbols = SymbolTable::new();
+    symbols.add("src", src);
+    symbols.add("dst", dst);
+    symbols.add("src_width", src_width);
 
-    // entry:
+    let entry = builder.new_block(llvm_func, "entry");
     builder.position_at_end(entry);
     let y_max = builder.trunc(src_height, builder.type_i32());
     let x_max = builder.trunc(src_width, builder.type_i32());
+    symbols.add("y_max", y_max);
+    symbols.add("x_max", x_max);
+
+    let generate_y_body = |symbols| {
+        generate_x_loop(&builder, func, llvm_func, symbols)
+    };
+    generate_y_loop(&builder, llvm_func, &mut symbols, generate_y_body);
+
+    builder.ret_void();
+    Module::new(module)
+}
+
+fn generate_y_loop<'s>(
+    builder: &Builder,
+    llvm_func: LLVMValueRef,
+    symbols: &'s mut SymbolTable,
+    mut generate_body: impl FnMut(&'s mut SymbolTable)
+) {
+    let pre_header = builder.get_insert_block();
+
+    let y_header = builder.new_block(llvm_func, "y.header");
+    let y_loop = builder.new_block(llvm_func, "y.loop");
+    let y_after = builder.new_block(llvm_func, "y.after");
+
+    builder.position_at_end(pre_header);
     builder.br(y_header);
+
+    // load symbols
+    let y_max = symbols.get("y_max");
+
     // y.header:
     builder.position_at_end(y_header);
     let no_rows = builder.icmp_eq(y_max, builder.const_i32(0));
@@ -175,8 +231,39 @@ pub fn create_process_image_module(context: &Context, func: &Func) -> Module {
     // y.loop:
     builder.position_at_end(y_loop);
     let y = builder.build_phi(builder.type_i32(), "y");
+    symbols.add("y", y);
     builder.add_phi_incoming(y, builder.const_i32(0), y_header);
+    generate_body(symbols);
+    let y_next = builder.add(y, builder.const_i32(1));
+    builder.add_phi_incoming(y, y_next, builder.get_insert_block());
+    let y_continue = builder.icmp_slt(y_next, y_max);
+    builder.cond_br(y_continue, y_loop, y_after);
+    // y.after:
+    builder.position_at_end(y_after);
+}
+
+fn generate_x_loop(
+    builder: &Builder,
+    func: &Func,
+    llvm_func: LLVMValueRef,
+    symbols: &mut SymbolTable
+) {
+    let pre_header = builder.get_insert_block();
+
+    let x_header = builder.new_block(llvm_func, "x.header");
+    let x_loop = builder.new_block(llvm_func, "x.loop");
+    let x_after = builder.new_block(llvm_func, "x.after");
+
+    builder.position_at_end(pre_header);
     builder.br(x_header);
+
+    // load symbols
+    let x_max = symbols.get("x_max");
+    let y = symbols.get("y");
+    let src = symbols.get("src");
+    let dst = symbols.get("dst");
+    let src_width = symbols.get("src_width");
+
     // x.header:
     builder.position_at_end(x_header);
     let no_cols = builder.icmp_eq(x_max, builder.const_i32(0));
@@ -190,15 +277,5 @@ pub fn create_process_image_module(context: &Context, func: &Func) -> Module {
     builder.add_phi_incoming(x, x_next, builder.get_insert_block());
     let x_continue = builder.icmp_slt(x_next, x_max);
     builder.cond_br(x_continue, x_loop, x_after);
-    // x.after:
     builder.position_at_end(x_after);
-    let y_next = builder.add(y, builder.const_i32(1));
-    builder.add_phi_incoming(y, y_next, builder.get_insert_block());
-    let y_continue = builder.icmp_slt(y_next, y_max);
-    builder.cond_br(y_continue, y_loop, y_after);
-    // y.after:
-    builder.position_at_end(y_after);
-    builder.ret_void();
-
-    Module::new(module)
 }
