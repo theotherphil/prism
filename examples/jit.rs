3793f0b2c98aced2193bb45e9434e03baf4bc6ab
Phil Ellison <phil.j.ellison@gmail.com>
2018-12-31 00:28:20 +0000

    move sum example into separate file

--- examples/jit.rs
+++ examples/jit.rs
@@ -6,10 +6,10 @@
 extern crate llvm_sys as llvm;
 
 use std::mem;
-use llvm::prelude::*;
 use llvm::core::*;
 use prism::*;
 use prism::codegen::*;
+use structopt::StructOpt;
 
 macro_rules! log_action {
     ($name:expr, $action:expr) => {{
@@ -20,28 +20,6 @@ macro_rules! log_action {
         println!(": COMPLETE");
         r
     }};
-}
-
-const SUM_IR: &str = "define i64 @sum(i64, i64, i64) {
-entry:
-    %sum.1 = add i64 %0, %1
-    %sum.2 = add i64 %sum.1, %2
-    ret i64 %sum.2
-}";
-
-fn create_sum_module_via_builder(context: &Context) -> LLVMModuleRef {
-    let module = context.new_module("sum");
-    let builder = Builder::new(context);
-    let i64t = builder.type_i64();
-    let function_type = builder.func_type(i64t, &mut [i64t, i64t, i64t]);
-    let function = builder.add_func(module, "sum", function_type);
-    let _ = builder.new_block(function, "entry");
-    let params = builder.get_params(function);
-    let (x, y, z) = (params[0], params[1], params[2]);
-    let sum = builder.add(x, y);
-    let sum = builder.add(sum, z);
-    builder.ret(sum);
-    module
 }
 
 const PROCESS_IMAGE_IR: &str = "define void @process_image(
@@ -95,9 +73,9 @@ y.for.end:
   ret void
 }";
 
-fn run_process_image_example(context: &Context, codegen: Codegen) {
+fn run_process_image(context: &Context, codegen: Codegen) {
     unsafe {
-        let def = read("in", x(), y()) + 1;
+        let def = read("in", x(), y()) + 3;
         let func = Func::new("out", &def);
         let module = match codegen {
             Codegen::Handwritten => create_module_from_handwritten_ir(context, PROCESS_IMAGE_IR),
@@ -105,6 +83,9 @@ fn run_process_image_example(context: &Context, codegen: Codegen) {
             //      "Unable to copy EFLAGS physical register!"
             // Searching for this produces a few LLVM bug reports, but it's also very possible
             // that I've messed something up
+            //
+            // EDIT: the optimisation step below fixes the compilation failure, which makes me
+            // EDIT: more suspicious that this is an LLVM bug
             Codegen::Builder => create_process_image_module(context, &func)
         };
         // Dump the module as IR to stdout.
@@ -124,50 +105,21 @@ fn run_process_image_example(context: &Context, codegen: Codegen) {
             "Function creation",
             || mem::transmute(engine.get_func_addr("process_image"))
         );
-        let x = gray_image!(1, 2, 3; 4, 5, 6; 7, 8, 9);
-        let mut y = GrayImage::new(3, 3);
+        let src = gray_image!(1, 2, 3; 4, 5, 6; 7, 8, 9);
+        let mut dst = GrayImage::new(3, 3);
         log_action! (
             "Function execution",
-            || f(x.buffer.as_ptr(), 3, 3, y.buffer.as_mut_ptr(), 3, 3)
+            || f(src.buffer.as_ptr(), 3, 3, dst.buffer.as_mut_ptr(), 3, 3)
         );
-        println!("map({}, {:?}) = {:?}", func.pretty_print(), x, y);
-    }
-}
-
-fn run_sum_example(context: &Context, codegen: Codegen) {
-    unsafe {
-        let module = match codegen {
-            Codegen::Handwritten => create_module_from_handwritten_ir(context, SUM_IR),
-            Codegen::Builder => create_sum_module_via_builder(context),
-        };
-        // Dump the module as IR to stdout.
-        LLVMDumpModule(module);
-
-        let engine = ExecutionEngine::new(module);
-
-        let addr = engine.get_func_addr("sum");
-        let f: extern "C" fn(u64, u64, u64) -> u64 = mem::transmute(addr);
-        let (x, y, z) = (1, 1, 1);
-        let res = f(x, y, z);
-        println!("{} + {} + {} = {}", x, y, z, res);
+        println!("Func: {}", func.pretty_print());
+        println!("src: {:?}", src);
+        println!("dst: {:?}", dst);
     }
 }
 
 /// Whether to use handwritten IR or an LLVM builder
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
-enum Codegen {
-    Handwritten,
-    Builder
-}
-
-/// Which example to use
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-enum Example {
-    Sum,
-    ProcessImage
-}
-
-use structopt::StructOpt;
+enum Codegen { Handwritten, Builder }
 
 fn codegen_from_str(d: &str) -> Codegen {
     match d {
@@ -177,19 +129,8 @@ fn codegen_from_str(d: &str) -> Codegen {
     }
 }
 
-fn example_from_str(d: &str) -> Example {
-    match d {
-        "sum" => Example::Sum,
-        "image" => Example::ProcessImage,
-        _ => panic!("invalid example flag")
-    }
-}
-
 #[derive(StructOpt, Debug)]
 struct Opts {
-    #[structopt(short = "e", long = "example")]
-    example: String,
-
     #[structopt(short = "c", long = "codegen")]
     codegen: String,
 }
@@ -197,13 +138,8 @@ struct Opts {
 fn main() {
     initialise_llvm_jit();
     let context = Context::new();
-    
     let opts = Opts::from_args();
-    let example = example_from_str(&opts.example);
     let codegen = codegen_from_str(&opts.codegen);
-    match example {
-        Example::Sum => run_sum_example(&context, codegen),
-        Example::ProcessImage => run_process_image_example(&context, codegen)
-    };
+    run_process_image(&context, codegen);
 }
 
---
+++ examples/sum.rs
@@ -0,0 +1,61 @@
+//! Reproduction of the jit-function example from llvm-sys but using
+//! this crate's Builder type.
+
+extern crate llvm_sys as llvm;
+
+use std::mem;
+use llvm::prelude::*;
+use llvm::core::*;
+use prism::codegen::*;
+
+const SUM_IR: &str = "define i64 @sum(i64, i64, i64) {
+entry:
+    %sum.1 = add i64 %0, %1
+    %sum.2 = add i64 %sum.1, %2
+    ret i64 %sum.2
+}";
+
+fn create_sum_module_via_builder(context: &Context) -> LLVMModuleRef {
+    let module = context.new_module("sum_builder");
+    let builder = Builder::new(context);
+    let i64t = builder.type_i64();
+    let function_type = builder.func_type(i64t, &mut [i64t, i64t, i64t]);
+    let function = builder.add_func(module, "sum", function_type);
+    let _ = builder.new_block(function, "entry");
+    let params = builder.get_params(function);
+    let (x, y, z) = (params[0], params[1], params[2]);
+    let sum = builder.add(x, y);
+    let sum = builder.add(sum, z);
+    builder.ret(sum);
+    module
+}
+
+fn run_sum(context: &Context, codegen: Codegen) {
+    println!("*** Running {:?}\n", codegen);
+    unsafe {
+        let module = match codegen {
+            Codegen::Handwritten => create_module_from_handwritten_ir(context, SUM_IR),
+            Codegen::Builder => create_sum_module_via_builder(context),
+        };
+        println!("** Module IR:");
+        LLVMDumpModule(module);
+
+        let engine = ExecutionEngine::new(module);
+
+        let addr = engine.get_func_addr("sum");
+        let f: extern "C" fn(u64, u64, u64) -> u64 = mem::transmute(addr);
+        let (x, y, z) = (1, 1, 1);
+        let res = f(x, y, z);
+        println!("{} + {} + {} = {}", x, y, z, res);
+    }
+}
+
+/// Whether to use handwritten IR or an LLVM builder
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+enum Codegen { Handwritten, Builde }
+
+fn main() {
+    initialise_llvm_jit();
+    run_sum(&Context::new(), Codegen::Handwritten);
+    run_sum(&Context::new(), Codegen::Builder);
+}
