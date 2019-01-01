222ac567f1f606824d3209f5711ce7715d7d8e37
Phil Ellison <phil.j.ellison@gmail.com>
2019-01-01 23:43:20 +0000

    remove a bit more boilerplate

--- src/processor.rs
+++ src/processor.rs
@@ -9,31 +9,19 @@ use std::mem;
 
 pub struct Processor {
     // TODO: needs lifetimes - can't use this after the execution engine is dropped
-    function: FunctionPointer,
+    function_pointer: u64,
     inputs: Vec<String>,
     outputs: Vec<String>
 }
 
 impl Processor {
-    pub fn new(graph: &Graph, addr: u64) -> Processor {
-        let function = unsafe {
-            match (graph.inputs().len(), graph.outputs().len()) {
-                (1, 1) => FunctionPointer::OneInOneOut(mem::transmute(addr)),
-                (1, 2) => FunctionPointer::OneInTwoOut(mem::transmute(addr)),
-                (2, 1) => FunctionPointer::TwoInOneOut(mem::transmute(addr)),
-                (2, 2) => FunctionPointer::TwoInTwoOut(mem::transmute(addr)),
-                (_, _) => panic!("Unsupported signature")
-            }
-        };
-        Processor {
-            function,
-            inputs: graph.inputs().to_vec(),
-            outputs: graph.outputs().to_vec()
-        }
+    pub fn new(graph: &Graph, function_pointer: u64) -> Processor {
+        let inputs = graph.inputs().to_vec();
+        let outputs = graph.outputs().to_vec();
+        Processor { function_pointer, inputs, outputs }
     }
 
-    pub fn process(&self, inputs: &[(&Source, &GrayImage)]
-    ) -> HashMap<String, GrayImage> {
+    pub fn process(&self, inputs: &[(&Source, &GrayImage)]) -> HashMap<String, GrayImage> {
         for source in &self.inputs {
             match inputs.iter().find(|i| &i.0.name == source) {
                 None => panic!(
@@ -52,75 +40,72 @@ impl Processor {
             .map(|name| (name.clone(), GrayImage::new(w, h)))
             .collect();
 
-        match self.function {
-            FunctionPointer::OneInOneOut(f) => {
+        match (self.inputs.len(), self.outputs.len()) {
+            (1, 1) => {
                 let i0 = &inputs[0].1;
                 let r0 = &mut calculated_images[0].1;
+                let f: extern "C" fn(
+                    *const u8, usize, usize,
+                    *mut u8, usize, usize
+                ) = unsafe { mem::transmute(self.function_pointer) };
                 f(
                     i0.buffer.as_ptr(), i0.width, i0.height,
                     r0.buffer.as_mut_ptr(), r0.width, r0.height
                 );
             },
-            FunctionPointer::OneInTwoOut(f) => {
+            (1, 2) => {
                 let i0 = &inputs[0].1;
                 let (rl, rr) = calculated_images.split_at_mut(1);
                 let r0 = &mut rl[0].1;
                 let r1 = &mut rr[0].1;
+                let f: extern "C" fn(
+                    *const u8, usize, usize,
+                    *mut u8, usize, usize,
+                    *mut u8, usize, usize
+                ) = unsafe { mem::transmute(self.function_pointer) };
                 f(
                     i0.buffer.as_ptr(), i0.width, i0.height,
                     r0.buffer.as_mut_ptr(), r0.width, r0.height,
                     r1.buffer.as_mut_ptr(), r1.width, r1.height
                 );
             },
-            FunctionPointer::TwoInOneOut(f) => {
+            (2, 1) => {
                 let i0 = &inputs[0].1;
                 let i1 = &inputs[0].1;
                 let r0 = &mut calculated_images[0].1;
+                let f: extern "C" fn(
+                    *const u8, usize, usize,
+                    *const u8, usize, usize,
+                    *mut u8, usize, usize
+                ) = unsafe { mem::transmute(self.function_pointer) };
                 f(
                     i0.buffer.as_ptr(), i0.width, i0.height,
                     i1.buffer.as_ptr(), i1.width, i1.height,
                     r0.buffer.as_mut_ptr(), r0.width, r0.height
                 );
             },
-            FunctionPointer::TwoInTwoOut(f) => {
+            (2, 2) => {
                 let i0 = &inputs[0].1;
                 let i1 = &inputs[0].1;
                 let (rl, rr) = calculated_images.split_at_mut(1);
                 let r0 = &mut rl[0].1;
                 let r1 = &mut rr[0].1;
+                let f: extern "C" fn(
+                    *const u8, usize, usize,
+                    *const u8, usize, usize,
+                    *mut u8, usize, usize,
+                    *mut u8, usize, usize
+                ) = unsafe { mem::transmute(self.function_pointer) };
                 f(
                     i0.buffer.as_ptr(), i0.width, i0.height,
                     i1.buffer.as_ptr(), i1.width, i1.height,
                     r0.buffer.as_mut_ptr(), r0.width, r0.height,
                     r1.buffer.as_mut_ptr(), r1.width, r1.height
                 );
-            }
+            },
+            (_, _) => panic!("Unsupported signature")
         };
 
         calculated_images.into_iter().collect()
     }
-}
-
-// Will do for now...
-pub enum FunctionPointer {
-    OneInOneOut(extern "C" fn(
-        *const u8, usize, usize,
-        *mut u8, usize, usize
-    )),
-    OneInTwoOut(extern "C" fn(
-        *const u8, usize, usize,
-        *mut u8, usize, usize,
-        *mut u8, usize, usize
-    )),
-    TwoInOneOut(extern "C" fn(
-        *const u8, usize, usize,
-        *const u8, usize, usize,
-        *mut u8, usize, usize
-    )),
-    TwoInTwoOut(extern "C" fn(
-        *const u8, usize, usize,
-        *const u8, usize, usize,
-        *mut u8, usize, usize,
-        *mut u8, usize, usize
-    )),
 }
